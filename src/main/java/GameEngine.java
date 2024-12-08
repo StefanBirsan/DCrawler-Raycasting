3d1983cc3f505593753b0efcb29eb53202b13c40
Stefan Birsan <stefbir10@gmail.com>
2024-12-08 23:47:30 +0200

    🎆 First commit

---
+++ src/main/java/GameEngine.java
@@ -0,0 +1,15 @@
+import javafx.geometry.Point2D;
+import javafx.util.Pair;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class GameEngine extends JFrame {
+
+    protected void start() {
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main  extends GameEngine {
+    public static void main(String[] args) {
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.start();
+    }
+}
